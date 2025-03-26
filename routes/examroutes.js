51b594e0b2e9f7c4414adfd108b1e5287942acfa
unknown <YY111@ccs.local>
2025-03-26 17:31:30 +0800

    handling get routes (igot)

--- routes/examroutes.js
+++ routes/examroutes.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const router = express.Router();
 
+
+// Hardcoded list of exams
+const users = [
+    { id: 1, name: "Alice", role: "Student" },
+    { id: 2, name: "Bob", role: "Instructor" },
+    { id: 3, name: "Charlie", role: "Admin" }
+];
+
 router.get("/", (req, res) => {
-    res.json({ message: "Group <your_group> API" });
+    res.json({ message: "Group H API" });
+});
+
+
+// get exams users
+router.get('/exams', (req, res) => {
+       res.json(users)
 });
 
 module.exports = router;
